2bd1f2bde93da9c69ddfcac9a07a1feb4206ac4f
LuaDeveloped <luadeveloped@gmail.com>
2024-10-02 13:35:20 -0400

    Added a test PHP file to make sure the server is running

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// PHP runs on the server
+$username = ""; // Backend logic
+$password = "";
+// Generate the frontend HTML
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head><title>LOGIN</title></head>";
+echo "<body>";
+echo "<h1>Aiden is a pussy<h1>";
+echo "<h1>USERNAME: </h1>";
+echo "<h1>PASSWORD: </h1>";
+echo "</body>";
+echo "</html>";
+?>
